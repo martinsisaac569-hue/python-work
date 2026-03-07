f1668819af9f9b72b86b03ada4b8afbb088a5b4a
topline19 <gomesisaac290@gmail.com>
2026-03-07 12:21:55 -0300

    Operations

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+"""
+n1 = input("digite o primeiro número:")
+n2 = input("digite o segundo número:")
+result= n1+n2
+print(result)
+
+n1= int ("digit n1:"input)
+n1= int(input("digite n1:"))
+n2= int(input("digite n2:"))
+result= n1+n2
+print(result)
+"""
+n1 = float(input("digite o número 1:"))
+n2 = float(input("digite o número 2:"))
+result = n1 + n2
+result1= int(result)
+print(result1)
